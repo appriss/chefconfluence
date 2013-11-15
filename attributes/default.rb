aac9bf21ad42e8483236eab5f370fb21b8eaa4d7
David Sledge <dsledge@appriss.com>
2013-11-15 16:25:23 -0500

    Initial checkin of the OpsCode Confluence Atlassian  cookbook

---
+++ attributes/default.rb
@@ -0,0 +1,51 @@
+#
+# Cookbook Name:: confluence
+# Attributes:: confluence
+#
+# Copyright 2008-2011, Opscode, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# The openssl cookbook supplies the secure_password library to generate random passwords
+default[:confluence][:virtual_host_name]  = "confluence.#{domain}"
+default[:confluence][:virtual_host_alias] = "confluence.#{domain}"
+# type-version-standalone
+default[:confluence][:base_name]	    = "atlassian-confluence"
+default[:confluence][:version]           = "2.1.1"
+default[:confluence][:install_path]      = "/opt/confluence"
+default[:confluence][:home]              = "/var/lib/confluence"
+default[:confluence][:source]            = "http://www.atlassian.com/software/confluence/downloads/binary/#{node[:confluence][:base_name]}-#{node[:confluence][:version]}.tar.gz"
+default[:confluence][:run_as]          = "confluence"
+default[:confluence][:min_mem]	    = 256
+default[:confluence][:max_mem]	    = 512
+default[:confluence][:ssl]		    = true
+default[:confluence][:database][:type]   = "mysql"
+default[:confluence][:database][:host]     = "localhost"
+default[:confluence][:database][:user]     = "confluence"
+default[:confluence][:database][:name]     = "confluence"
+default[:confluence][:service][:type]      = "jsw"
+if node[:opsworks][:instance][:architecture]
+  default[:confluence][:jsw][:arch]          = node[:opsworks][:instance][:architecture].gsub!(/_/,"-")
+else
+  default[:confluence][:jsw][:arch]          = node[:kernel][:machine].gsub!(/_/,"-")
+end
+default[:confluence][:jsw][:base_name]     = "wrapper-linux-#{node[:confluence][:jsw][:arch]}"
+default[:confluence][:jsw][:version]       = "3.5.20"
+default[:confluence][:jsw][:install_path]  = ::File.join(node[:confluence][:install_path],"#{node[:confluence][:base_name]}")
+default[:confluence][:jsw][:source]        = "http://wrapper.tanukisoftware.com/download/#{node[:confluence][:jsw][:version]}/wrapper-linux-#{node[:confluence][:jsw][:arch]}-#{node[:confluence][:jsw][:version]}.tar.gz"
+# Confluence doesn't support OpenJDK http://confluence.atlassian.com/browse/CONF-16431
+# FIXME: There are some hardcoded paths like JAVA_HOME
+set[:java][:install_flavor]    = "oracle"
+set[:oracledb][:jdbc][:install_dir] = ::File.join(node[:confluence][:install_path],node[:confluence][:base_name],"lib")
+
---
+++ recipes/default.rb
@@ -0,0 +1,159 @@
+
+#
+# Cookbook Name:: confluence
+# Recipe:: default
+#
+# Copyright 2013, Appriss Inc.
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+require 'uri'
+include_recipe 'java'
+#include_recipe 'apache2'
+#include_recipe 'apache2::mod_rewrite'
+#include_recipe 'apache2::mod_proxy'
+#include_recipe 'apache2::mod_ssl'
+include_recipe 'labrea'
+
+confluence_base_dir = File.join(node[:confluence][:install_path],node[:confluence][:base_name])
+
+# Create a system user account on the server to run the Atlassian Confluence server
+user node[:confluence][:run_as] do
+  system true
+  shell  '/bin/bash'
+  action :create
+end
+
+# Create a home directory for the Atlassian Confluence user
+directory node[:confluence][:home] do
+  owner node[:confluence][:run_as]
+end
+
+# Install or Update the Atlassian Confluence package
+labrea "atlassian-confluence" do
+  source node[:confluence][:source]
+  version node[:confluence][:version]
+  install_dir node[:confluence][:install_path]
+  config_files [File.join("atlassian-confluence-#{node[:confluence][:version]}","confluence","WEB-INF","classes","confluence-init.properties"),
+	        File.join("atlassian-confluence-#{node[:confluence][:version]}","conf","server.xml")]
+  notifies :run, "execute[configure confluence permissions]", :immediately
+end
+
+# Install database drivers if needed
+if node[:confluence][:database][:type] == "oracle"
+  uri = ::URI.parse(node[:confluence][:database][:driver_url])
+  if uri.scheme == "s3"
+    Chef::Log.info ("URI #{node[:confluence][:database][:driver_url]}")
+    Chef::Log.info ("HOST #{uri.host}")
+    Chef::Log.info ("PATH #{uri.path}")
+    s3_file ::File.join(confluence_base_dir,"lib","oracle_jdbc_driver.jar") do
+      bucket uri.host
+      remote_path uri.path
+      owner node[:confluence][:run_as]
+      #mode 0644
+      action :create
+    end
+  else
+    remote_file ::File.join(confluence_base_dir,"lib","oracle_jdbc_driver.jar") do
+      source node[:confluence][:database][:driver_url]
+      owner node[:confluence][:run_as]
+      mode 0644
+      action :create
+    end
+  end
+end
+
+
+# Set the permissions of the Atlassian Confluence directory
+execute "configure confluence permissions" do
+  command "chown -R #{node[:confluence][:run_as]} #{node[:confluence][:install_path]}"
+  action :nothing
+end
+
+# Install main config file
+template ::File.join(confluence_base_dir,"confluence","WEB-INF","classes","confluence-init.properties") do
+  owner node[:confluence][:run_as]
+  source "confluence-init.properties.erb"
+  mode 0644
+end
+
+# Add the server.xml configuration for Crowd using the erb template
+template ::File.join(confluence_base_dir,"conf","server.xml") do
+  owner node[:confluence][:run_as]
+  source "server.xml.erb"
+  mode 0644
+end
+
+# Install service wrapper
+
+wrapper_home = File.join(confluence_base_dir,node[:confluence][:jsw][:base_name])
+
+labrea node[:confluence][:jsw][:base_name] do
+  source node[:confluence][:jsw][:source]
+  version node[:confluence][:jsw][:version]
+  install_dir node[:confluence][:jsw][:install_path]
+  config_files [File.join("#{node[:confluence][:jsw][:base_name]}-#{node[:confluence][:jsw][:version]}","conf","wrapper.conf")]
+  notifies :run, "execute[configure wrapper permissions]", :immediately
+end
+
+# Configure wrapper permissions
+execute "configure wrapper permissions" do
+  command "chown -R #{node[:confluence][:run_as]} #{wrapper_home} #{wrapper_home}/*"
+  action :nothing
+end
+
+# Configure wrapper
+template File.join(wrapper_home,"conf","wrapper.conf") do
+  owner node[:confluence][:run_as]
+  source "wrapper.conf.erb"
+  mode 0644
+  variables({
+    :wrapper_home => wrapper_home,
+    :confluence_base_dir => confluence_base_dir
+  })
+end
+
+# Create wrapper startup script
+template File.join(wrapper_home,"bin","confluence") do
+  owner node[:confluence][:run_as]
+  source "confluence-startup.erb"
+  mode 0755
+  variables({
+    :wrapper_home => wrapper_home
+  })
+  notifies :run, "execute[install startup script]", :immediately
+end
+
+execute "install startup script" do
+  command "#{::File.join(wrapper_home,"bin","confluence")} install"
+  action :nothing
+  returns [0,1]
+  notifies :restart, "service[confluence]", :immediately
+end
+
+service "confluence" do
+  action :nothing
+end
+
+# Enable the Apache2 proxy_http module
+#execute "a2enmod proxy_http" do
+#  command "/usr/sbin/a2enmod proxy_http"
+#  notifies :restart, resources(:service => "apache2")
+#  action :run
+#end
+
+# Add the setenv.sh environment script using the erb template
+#template File.join("#{node[:confluence][:install_path]}/atlassian-confluence","/bin/setenv.sh") do
+#  owner node[:confluence][:run_as]
+#  source "setenv.sh.erb"
+#  mode 0644
+#end
+
+# Setup the virtualhost for Apache
+#web_app "confluence" do
+#  docroot File.join("#{node[:confluence][:install_path]}/atlassian-confluence","/") 
+#  template "confluence.vhost.erb"
+#  server_name node[:fqdn]
+#  server_aliases [node[:hostname], "confluence"]
+#end
